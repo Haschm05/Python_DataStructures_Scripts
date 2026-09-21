d0f2e074a91de32a5ec0522e2acda406530d673e
Haschm05 <haydensemail@sio.midco.net>
2026-09-21 11:34:05 -0500

    Create CryptoMessages.py

---
+++ labs/CryptoMessages.py
@@ -0,0 +1,40 @@
+“””
+Project documentation
+“””
+Import base64
+Initialize file and variables()
+Tuple = initialization data 1
+Tuple = initializationdata 2
+
+Set = id 1
+Set = id 2
+Set = id 3
+
+If file not exists:
+		Makefilename
+Else:
+return
+
+
+
+Generate random data()
+	Random {
+
+
+	}
+	
+	List = random.random(Random)
+
+Encode data()
+	Base64.encode?
+Write information()
+	Write list to file
+
+Decode data()
+	Base 64.decode?
+
+Read information()
+	Dict = decode information()
+
+Display information()
+	Print(dict)
